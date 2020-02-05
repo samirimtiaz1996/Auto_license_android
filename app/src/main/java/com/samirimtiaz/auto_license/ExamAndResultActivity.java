07784eca3b1c96893fe70d00bcfb152f485d4615
Samir Imtiaz <samirimtiaz1234@gmail.com>
2020-02-06 04:40:34 +0600

    readded

--- app/src/main/java/com/samirimtiaz/auto_license/ExamAndResultActivity.java
+++ app/src/main/java/com/samirimtiaz/auto_license/ExamAndResultActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ExamAndResult extends AppCompatActivity {
+public class ExamAndResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
