8023b6b87f2016fec49fe6b2cbd5e9f99023f494
Samir Imtiaz <samirimtiaz1234@gmail.com>
2020-02-10 18:59:34 +0600

    activity added

---
+++ app/src/main/java/com/samirimtiaz/auto_license/CarDrivingTestActivity.java
@@ -0,0 +1,14 @@
+package com.samirimtiaz.auto_license;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CarDrivingTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_car_driving_test);
+    }
+}
